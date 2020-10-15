71d02aed177677023a2774ef3ad23558b0fee940
xintrea <xintrea@gmail.com>
2020-10-15 12:57:01 +0300

    Сделено рекурсивное получение имен файлов для распознавания из заданной директории

--- documentator.php
+++ documentator.php
@@ -1,7 +1,7 @@
 <?php
 
 // Каталог с документами, которые надо распознать
-$docDir='../Компьютерра/Komputerra/2009';  
+$docDir='../Компьютерра/Komputerra';  
 
 // Служебный каталог, куда будут распаковываться изображения для последующего ORC
 $extractDir='./extract';
@@ -28,13 +28,69 @@ class Documentator
     }
 
     
+    // Запуск процесса распознавания всех файлов
     public function run()
     {
         echo 'Documentator v.'.$this->version."\n";
         
         $this->createExtractDir();
+        
+        // Список полных путей к файлам распознаваемых документов
+        $files=$this->getFiles();
+        
+        // Рспознавание документов
+        foreach($files as $file)
+        {
+            echo 'Translate for '.$file."\n";
+            
+            $this->translate($file);
+            
+            echo 'Done'."\n";
+        }
+        
+        echo 'All files translated.'."\n";
     }
     
+    
+    protected function translate()
+    {
+        global $extractDir;
+        
+    }
+
+    
+    protected function getSupportFormats()
+    {
+        return array('pdf', 'fb2');
+    }
+
+
+    protected function getFiles()
+    {
+        global $docDir;
+        
+        $result=array();
+        
+        foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($docDir)) as $fileName)
+        {
+            // Исключение имен "." и ".."
+            if($fileName->isDir()) 
+            {
+                continue;
+            }
+            
+            if( !in_array( pathinfo( $fileName, PATHINFO_EXTENSION ), $this->getSupportFormats() ) )
+            {
+                echo 'Unavailavle file extention. '.$fileName."\n";
+                continue;
+            }
+
+            $result[]=$fileName;
+        }
+        
+        return $result;
+    }
+
     
     protected function createExtractDir()
     {
